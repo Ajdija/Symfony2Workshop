29917345cd8d2bd4f22d2ae2db9523f052bf29e7
Mateusz Grzesiukiewicz <ajdija@gmail.com>
2015-04-22 21:45:22 +0200

    UniqueEntity

--- src/Acme/ImageBundle/Entity/Image.php
+++ src/Acme/ImageBundle/Entity/Image.php
@@ -4,9 +4,11 @@ namespace Acme\ImageBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
  * Image
+ * @UniqueEntity("name")
  */
 class Image
 {
